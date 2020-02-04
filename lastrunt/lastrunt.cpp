34ad8d95f104029cf5c143aad4884d6a40d2b1b6
schmide <andrew@schmide.com>
2020-02-03 21:57:00 -0500

    Tested Sorting
    after testing it looks like good cases are
    
    sorted either direction
    sorted inside out
    
    worst case
    
    sorted outside in

--- lastrunt/lastrunt.cpp
+++ lastrunt/lastrunt.cpp
@@ -1,28 +1,33 @@
 ﻿// lastrunt.cpp  (c compliant)
-//
-// schmide's shifted version
-//
-// order of operations 
-// separate out each calculation
-// find the number with the highest base
-// shift numbers to that base
-// add and shift back
-//
-// a more useful version could work with 
-// 2 numbers at a time
 // 
-// overflow on doubles will happen around 
-// 2^2^14 = 1.1897314e+4932‬
+// after trying some shifts and adjustments
+// I found that simply ordering the operations 
+// to avoid pitfalls provides the best precision.
 //
-// further optimizations on precision would
-// involve sorting, alternating sign, 
-// pairing, variable shifts, etc
+// guess
+// optimal order would probably be magnitude greatest 
+// to least with alternating signs
+//
+// after testing it looks like good cases are
+//
+// sorted either direction
+// sorted inside out
+//
+// worst case 
+// 
+// sorted outside in
 
 #include <iostream>
 
 // f = 333.75 * b ^ 6 + a ^ 2 * (11 * a ^ 2 * b ^ 2 - b ^ 6 - 121 * b ^ 4 - 2) + 5.5 * b ^ 8 + (a / (2 * b))
 
-long double shift_fp(long double a, long double b)
+int compare_long_double(const void * a, const void * b)
+{
+   long double diff = *(long double*)a - *(long double*)b;
+   return diff == 0.0L ? 0 : diff < 0.0L ? -1 : 1;
+}
+
+long double ordered_fp(long double a, long double b)
 {
 #define NUM_NUMBERS 7   
    long double numbers[NUM_NUMBERS] = {
@@ -32,39 +37,79 @@ long double shift_fp(long double a, long double b)
       -121.0L * powl(b, 4.0L) * powl(a, 2.0L),
       -2.0L * powl(a, 2.0L),
       5.5L * powl(b, 8.0L),
-      a / (2.0 * b) // everything cancels out but this.
+      a / (2.0 * b) 
    };
 
-   printf("base numbers\n\n");
+   printf("  base numbers\n\n");
    int i = 0;
    do {
-      printf("numbers[%i] = %-+42.36Le\n", i, numbers[i]);
+      printf("  numbers[%i] = %-+42.36Le\n", i, numbers[i]);
    } while (++i < NUM_NUMBERS);
 
-   long double shifts[NUM_NUMBERS];
-   long double max_shift = 0;
-   printf("\n\npower 2 shifts\n\n");
-   i = 0;
-   do {
-      shifts[i] = powl(2.0L, (long double)(unsigned long)(logl(fabsl(numbers[i])) / logl(2.0L)));
-      printf("shifts[%i] = %-+42.36Le\n", i, shifts[i]);
-      max_shift = fmaxl(max_shift, shifts[i]);
-   } while (++i < NUM_NUMBERS);
-   printf("\nmax shift = %-+42.36Le\n", max_shift);
-
-   printf("\n\nadjusted shifts\n\n");
-   i = 0;
-   do {
-      shifts[i] /= max_shift;
-      printf("shifts[%i] = %-+42.36Le\n", i, shifts[i]);
-   } while (++i < NUM_NUMBERS);
-
-   printf("\n\nOutput\n\n");
+   printf("\n\n  original order\n\n");
    long double output = 0.0L;
    i = 0;
    do {
-      output = (output * shifts[i] + numbers[i] * shifts[i]) / shifts[i];
-      printf("output = %-+42.36Le\n", output);
+      output += numbers[i];
+      printf("  output = %-+42.36Le\n", output);
+   } while (++i < NUM_NUMBERS);
+
+   printf("\n\n  reverse order\n\n");
+   output = 0.0L;
+   i = 0;
+   do {
+      output += numbers[NUM_NUMBERS - 1 - i];
+      printf("  output = %-+42.36Le\n", output);
+   } while (++i < NUM_NUMBERS);
+
+   qsort(numbers, NUM_NUMBERS, sizeof(numbers[0]), compare_long_double);
+
+   printf("\n\n  sorted numbers\n\n");
+   i = 0;
+   do {
+      printf("  numbers[%i] = %-+42.36Le\n", i, numbers[i]);
+   } while (++i < NUM_NUMBERS);
+
+   printf("\n\n  sorted in order\n\n");
+   output = 0.0L;
+   i = 0;
+   do {
+      output += numbers[i];
+      printf("  output = %-+42.36Le\n", output);
+   } while (++i < NUM_NUMBERS);
+
+   printf("\n\n  sorted reverse order\n\n");
+   output = 0.0L;
+   i = 0;
+   do {
+      output += numbers[NUM_NUMBERS-1-i];
+      printf("  output = %-+42.36Le\n", output);
+   } while (++i < NUM_NUMBERS);
+
+   printf("\n\n  inside out\n\n");
+   output = 0.0L;
+   i = 0;
+   int upper = NUM_NUMBERS >> 1;
+   int lower = upper + 1;
+   do {
+      if (i & 1)
+         output += numbers[lower++];
+      else
+         output += numbers[upper--];
+      printf("  output = %-+42.36Le\n", output);
+   } while (++i < NUM_NUMBERS);
+
+   printf("\n\n  outside in\n\n");
+   output = 0.0L;
+   i = 0;
+   upper = NUM_NUMBERS - 1;
+   lower = 0;
+   do {
+      if (i & 1)
+         output += numbers[lower++];
+      else
+         output += numbers[upper--];
+      printf("  output = %-+42.36Le\n", output);
    } while (++i < NUM_NUMBERS);
    return output;
 }
@@ -73,5 +118,5 @@ int main()
 {
    // -0.827396059946821368141165095479816291999033115785
    // +1.172603940053178694924440605973359197e+00
-   double test = shift_fp(77617.0, 33096.0);
+   double test = ordered_fp(77617.0, 33096.0);
 }
