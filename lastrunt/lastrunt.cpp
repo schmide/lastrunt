51343031e9a75d02a315181d70ece99857ac132e
schmide <andrew@schmide.com>
2020-02-05 13:28:27 -0500

    fixed issue with negative split numbers

--- lastrunt/lastrunt.cpp
+++ lastrunt/lastrunt.cpp
@@ -51,6 +51,77 @@
 #define FLOAT_TYPE double
 #define POW_FUN pow
 
+FLOAT_TYPE shift_fp(FLOAT_TYPE a, FLOAT_TYPE b)
+{
+#define NUM_NUMBERS 7   
+   FLOAT_TYPE numbers[NUM_NUMBERS] = {
+      333.75 * pow(b, 6.0),
+      11.0 * pow(a, 4.0) * pow(b, 2.0),
+      -pow(a, 2.0) * pow(b, 6.0),
+      -121.0 * pow(b, 4.0) * pow(a, 2.0),
+      -2.0 * pow(a, 2.0),
+      5.5 * pow(b, 8.0),
+      a / (2.0 * b)
+   };
+
+   printf("  base numbers\n\n");
+   int i = 0;
+   do {
+      printf("  numbers[%i] = %-+42.36Le\n", i, numbers[i]);
+   } while (++i < NUM_NUMBERS);
+
+   printf("\n\n  split numbers\n\n");
+   FLOAT_TYPE high[NUM_NUMBERS];
+   FLOAT_TYPE low[NUM_NUMBERS];
+   FLOAT_TYPE lowlow[NUM_NUMBERS];
+   i = 0;
+   do {
+      FLOAT_TYPE temp;
+      bool negative = numbers[i] < 0.0;
+      temp = high[i] = negative ? -numbers[i] : numbers[i];
+      ((unsigned long *)&high[i])[0] = 0;
+      temp = low[i] = temp - high[i];
+      ((unsigned long *)&low[i])[0] = 0;
+      lowlow[i] = temp - low[i];
+      temp = negative ? -1.0 : 1.0;
+      printf("  high[%i] = %-+42.36Le\n", i, temp * high[i]);
+      printf("  low[%i] = %-+42.36Le\n", i, temp * low[i]);
+      printf("  lowlow[%i] = %-+42.36Le\n", i, temp * lowlow[i]);
+      printf("  check[%i] = %-+42.36Le\n", i, temp * (high[i] + low[i] + lowlow[i]));
+   } while (++i < NUM_NUMBERS);
+
+   FLOAT_TYPE shifts[NUM_NUMBERS];
+   FLOAT_TYPE max_shift = 0;
+   printf("\n\n  power 2 shifts\n\n");
+   i = 0;
+   do {
+      FLOAT_TYPE power_2 = log(fabsl(numbers[i])) / log(2.0);
+      shifts[i] = pow(2.0, (FLOAT_TYPE)(unsigned long)power_2);
+      printf("  shifts[%i] = %-+42.36Le power 2 = %.2f\n", i, shifts[i], power_2);
+      max_shift = fmaxl(max_shift, shifts[i]);
+   } while (++i < NUM_NUMBERS);
+   printf("\n  max shift = %-+42.36Le\n", max_shift);
+
+   printf("\n\n  adjusted shifts\n\n");
+   i = 0;
+   do {
+      shifts[i] /= max_shift;
+      printf("  shifts[%i] = %-+42.36Le\n", i, shifts[i]);
+   } while (++i < NUM_NUMBERS);
+
+   printf("\n\n  output\n\n");
+   FLOAT_TYPE output = 0.0;
+   i = 0;
+   do {
+      output += numbers[i];
+      printf("  output = %-+42.36Le\n", output);
+   } while (++i < NUM_NUMBERS);
+   output = -2.0 + numbers[6];
+   printf("\n  (answer)\n  -2.0 + numbers[6] = %-+42.36Le\n\n", output);
+
+   return output;
+}
+
 int compare_float_type(const void * a, const void * b)
 {
    FLOAT_TYPE diff = *(FLOAT_TYPE*)a - *(FLOAT_TYPE*)b;
@@ -146,77 +217,6 @@ FLOAT_TYPE ordered_fp(FLOAT_TYPE a, FLOAT_TYPE b)
 
    return output;
 }
-
-FLOAT_TYPE shift_fp(FLOAT_TYPE a, FLOAT_TYPE b)
-{
-#define NUM_NUMBERS 7   
-   FLOAT_TYPE numbers[NUM_NUMBERS] = {
-      333.75 * pow(b, 6.0),
-      11.0 * pow(a, 4.0) * pow(b, 2.0),
-      -pow(a, 2.0) * pow(b, 6.0),
-      -121.0 * pow(b, 4.0) * pow(a, 2.0),
-      -2.0 * pow(a, 2.0),
-      5.5 * pow(b, 8.0),
-      a / (2.0 * b)
-   };
-
-   printf("  base numbers\n\n");
-   int i = 0;
-   do {
-      printf("  numbers[%i] = %-+42.36Le\n", i, numbers[i]);
-   } while (++i < NUM_NUMBERS);
-
-   printf("\n\n  split numbers\n\n");
-   FLOAT_TYPE high[NUM_NUMBERS];
-   FLOAT_TYPE low[NUM_NUMBERS];
-   FLOAT_TYPE lowlow[NUM_NUMBERS];
-   i = 0;
-   do {
-      FLOAT_TYPE temp;
-      bool negative = numbers[i] < 0.0;
-      high[i] = numbers[i];
-      ((unsigned long *)&high[i])[0] = 0;
-      temp = low[i] = negative ? numbers[i] - high[i] : numbers[i] + high[i]; 
-      ((unsigned long *)&low[i])[0] = 0;
-      lowlow[i] = temp - low[i];
-      printf("  high[%i] = %-+42.36Le\n", i, high[i]);
-      printf("  low[%i] = %-+42.36Le\n", i, low[i]);
-      printf("  lowlow[%i] = %-+42.36Le\n", i, lowlow[i]);
-      printf("  check[%i] = %-+42.36Le\n", i, high[i] + low[i] + lowlow[i]);
-   } while (++i < NUM_NUMBERS);
-
-   FLOAT_TYPE shifts[NUM_NUMBERS];
-   FLOAT_TYPE max_shift = 0;
-   printf("\n\n  power 2 shifts\n\n");
-   i = 0;
-   do {
-      FLOAT_TYPE power_2 = log(fabsl(numbers[i])) / log(2.0);
-      shifts[i] = pow(2.0, (FLOAT_TYPE)(unsigned long)power_2);
-      printf("  shifts[%i] = %-+42.36Le power 2 = %.2f\n", i, shifts[i], power_2);
-      max_shift = fmaxl(max_shift, shifts[i]);
-   } while (++i < NUM_NUMBERS);
-   printf("\n  max shift = %-+42.36Le\n", max_shift);
-
-   printf("\n\n  adjusted shifts\n\n");
-   i = 0;
-   do {
-      shifts[i] /= max_shift;
-      printf("  shifts[%i] = %-+42.36Le\n", i, shifts[i]);
-   } while (++i < NUM_NUMBERS);
-
-   printf("\n\n  output\n\n");
-   FLOAT_TYPE output = 0.0;
-   i = 0;
-   do {
-      output += numbers[i]; 
-      printf("  output = %-+42.36Le\n", output);
-   } while (++i < NUM_NUMBERS);
-   output = -2.0 + numbers[6];
-   printf("\n  (answer)\n  -2.0 + numbers[6] = %-+42.36Le\n\n", output);
-
-   return output;
-}
-
 int main()
 {
    // -0.827396059946821368141165095479816291999033115785
